d4a7415806fda057684cec8c4d878986925b068f
orosy <hanei7632@gmail.com>
2021-11-15 21:15:26 +0900

    Add: 카운터를 useReducer 로 다시 구현하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 // import Greetings from "./Greetings";
 // import Counter from "./Counter";
-import MyForm from "./MyForm";
+// import MyForm from "./MyForm";
+import ReducerSample from "./ReducerSample";
 
 function App() {
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
-  return <MyForm onSubmit={onSubmit} />;
+  return <ReducerSample />;
 }
 
 export default App;
